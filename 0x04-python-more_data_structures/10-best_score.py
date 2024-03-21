2a3c6d0dafb1e683c27e1cb798ecdd1e9c04dcbd
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2024-03-21 02:54:10 +0000

    up

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,3 @@
-t_score(a_dictionary):
+#!/usr/bin/python3
+def best_score(a_dictionary):
     return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,2 +1,3 @@
-_dictionary):
+#!/usr/bin/python3
+def number_keys(a_dictionary):
     return len(a_dictionary)
