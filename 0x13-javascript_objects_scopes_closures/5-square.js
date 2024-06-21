8603be161e1174b89478b306f16a702bdad9d3ce
RAJACHAY <chaymae.rajafillah@usmba.ac.ma>
2024-06-20 18:34:21 -0700

    25

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,29 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-  rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-module.exports = class Square extends Rectangle {
+/*  class Square that defines a square and
+inherits from Rectangle of 4-rectangle.js */
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-};
+}
+module.exports = Square;
